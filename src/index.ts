0231745a9a223730677fb34cc55cffedc26be107
Aarush Karak <worldshaans@gmail.com>
2026-09-12 15:20:07 -0400

    Update vite-plugin: 2026-09-12 16:00 [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { type Plugin, type ResolvedConfig, normalizePath } from "vite";
-// Build: 2026-09-12 15:30:08 | run 1789241408
+// Build: 2026-09-12 16:00:24 | run 1789243224
 import { resolve, relative } from "path";
 import { writeFileSync, existsSync, mkdirSync } from "fs";
 
